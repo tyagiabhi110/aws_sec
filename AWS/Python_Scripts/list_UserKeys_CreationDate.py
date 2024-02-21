1f0173c04d0bf280e131aae697b2300107f21e73
Abhishek Tyagi <abhishek.tyagi@aveva.com>
2024-02-21 19:20:51 +0530

    Update script to create session fir multiple accounts

--- AWS/Python_Scripts/list_UserKeys_CreationDate.py
+++ AWS/Python_Scripts/list_UserKeys_CreationDate.py
@@ -1,23 +1,27 @@
 import boto3
-session = boto3.Session(profile_name="profile_name_here",region_name="eu-west-1")
-client = session.client('iam')
-response = client.list_users()
 
-# Iterate over the response to print usernames
-for user in response['Users']:
-    user_name = user['UserName']  # Assign the username to user_name variable
-    # print(user_name)  # Print the username
-    # For excluding users
-    if user_name == "<excluding_user_name>":
-        continue
-    else:
-        # Now you can use user_name to list access keys for each user
-        access_keys_response = client.list_access_keys(UserName=user_name)
+# Debug on/off
 
-        # Iterate over access keys metadata and extract creation date
-        for access_key in access_keys_response['AccessKeyMetadata']:
-            Create_Date = access_key['CreateDate']
-            User_Name = access_key['UserName']
-            print("UserName \t\t\t\t\t Creation_Date")
-            print("---------------------------------------------------")
-            print(User_Name,  "\t\t",  Create_Date)
+DEBUG = 0
+
+with open("../accounts.txt") as accounts:
+    for account in accounts:
+        try:
+            session = boto3.Session(profile_name=account.rstrip('\n').rstrip(),region_name="eu-west-1")
+            print(session)
+            client = session.client('iam')
+            response = client.list_users()
+            for user in response['Users']:
+                user_name= user['UserName']
+                if user_name == "CDOBuildUser":
+                    continue
+                else:
+                    access_key_response = client.list_access_keys(UserName=user_name)
+                    for key in access_key_response["AccessKeyMetadata"]:
+                        creation_time = key['CreateDate']
+                        username = key['UserName']
+                        print("User Name \t\t\t\t Access_Key_Creation_Date")
+                        print("---------------------------------------------------")
+                        print(username, "\t\t", creation_time)
+        except ClientError as e:
+                print("e")
